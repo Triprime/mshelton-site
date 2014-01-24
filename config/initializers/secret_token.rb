8939afd0d71a90a3ea500668829fc2177eeaf61c
Triprime <shelton_matthew@hotmail.com>
2014-01-23 19:22:50 -0700

    Initial Rails creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MsheltonSite::Application.config.secret_token = '87acf40dced1766b06e182af54bfbc970c8868e7bafd3809aa329c96ef814612076826b30b276c8e2d5a629b1fb09010a359e9c88e10db37c0aca1d82d685915'
